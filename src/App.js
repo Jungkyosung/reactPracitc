064d8fccee160b9f790a7e426b1eb570464a3b23
DESKTOP-QJ5VISR\jksin <jksin13@naver.com>
2023-02-21 01:23:10 +0900

    리액트 공식문서 튜토리얼 틱택토게임 만들기 #2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
+import { useState } from 'react';
+
+function Square({value, onSquareClick}) {
+
+  return (
+    <button className="square" onClick={onSquareClick} >{value}</button>
+  )
+}
+
+
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const valueOf3 = ['X', 'O', null];
+  const handlerClick = (i)=>{
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value={squares[0]} onSquareClick={()=>handlerClick(0)}/>
+        <Square value={squares[1]} onSquareClick={()=>handlerClick(1)}/>
+        <Square value={squares[2]} onSquareClick={()=>handlerClick(2)}/>
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value={squares[3]} onSquareClick={()=>handlerClick(3)}/>
+        <Square value={squares[4]} onSquareClick={()=>handlerClick(4)}/>
+        <Square value={squares[5]} onSquareClick={()=>handlerClick(5)}/>
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value={squares[6]} onSquareClick={()=>handlerClick(6)}/>
+        <Square value={squares[7]} onSquareClick={()=>handlerClick(7)}/>
+        <Square value={squares[8]} onSquareClick={()=>handlerClick(8)}/>
       </div>
     </>
   );
 }
+
